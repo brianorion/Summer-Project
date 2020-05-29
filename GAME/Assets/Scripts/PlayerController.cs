6b6e0e693f41f48c17927c1a904d597787e8e5bd
brianorion <brianplease1@gmail.com>
2020-05-29 12:36:43 -0400

    Added a player that can now move on a plane.

---
+++ GAME/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 velocity = rb.velocity;
+
+        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        velocity.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        rb.velocity = velocity;
+    }
+}
