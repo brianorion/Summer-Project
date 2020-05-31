51f9bd1b442aed4a17760912b5c42de3cdaab5d3
brianorion <brianplease1@gmail.com>
2020-05-31 16:14:38 -0400

    Added a jump function, grab function, and clamped the players first person camera.

--- GAME/Assets/Scripts/PlayerController.cs
+++ GAME/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Basic Player stats")]
     [SerializeField]
     private float speed = 1f;
     [SerializeField]
     private float rotationSensitivity = 1f;
     [SerializeField]
+    private float jumpForce = 1f;
+    [SerializeField]
+    private float maximumCameraRotation = 90f;
+    [SerializeField]
+    private float pickUpDistance = 5f;
+
+    [Header("Associated Objects")]
+    [SerializeField]
     private GameObject firstPersonCamera;
+    [SerializeField]
+    private Transform guide;
 
     private Rigidbody rb;
 
+    // these are all of the item pick up
+    private Rigidbody itemRb;
+    private Transform itemTransform;
+
     private bool changeState = false;
-    
+    private bool canPickUp = false;
+    private Vector3 jumpVector;
+
     public bool inFirstPerson = true;
 
 
@@ -22,6 +39,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         firstPersonCamera.SetActive(inFirstPerson);
+        jumpVector = new Vector3(0f, jumpForce, 0f);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -48,11 +67,14 @@ public class PlayerController : MonoBehaviour
     {
         if (firstPerson)
         {
+            Cursor.lockState = CursorLockMode.Locked;
             PlayerRotate();
+            ClickAndDrag();
             firstPersonCamera.SetActive(firstPerson);
         }
         else
         {
+            // TODO maybe in third person mode there might be something different. 
             ThirdPersonMode();
             firstPersonCamera.SetActive(firstPerson);
         }
@@ -87,6 +109,13 @@ public class PlayerController : MonoBehaviour
 
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
         }
+
+        // the jumping functionality doesn't depend on whether or not the player is in first person or in third person. Thus, I am doing it outside of the if statement. 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // At the moment, a person can have infinite jumps. Too lazy to fix it so I will do it later. 
+            rb.AddForce(jumpVector, ForceMode.Impulse);
+        }
     }
 
     private void PlayerRotate()
@@ -104,15 +133,59 @@ public class PlayerController : MonoBehaviour
     
     private void CameraRotate()
     {
+        
         float xRot = Input.GetAxis("Mouse Y");
 
         Vector3 rotation = new Vector3(xRot, 0f, 0f) * rotationSensitivity;
 
         firstPersonCamera.transform.Rotate(-rotation);
+        float currentX = firstPersonCamera.transform.localEulerAngles.x;
+        currentX = Mathf.Clamp(currentX, -270f, maximumCameraRotation);
+
+        firstPersonCamera.transform.localEulerAngles = new Vector3(currentX, 0f, 0f);
     }
 
     private void ThirdPersonMode()
     {
+        Cursor.lockState = CursorLockMode.None;
+    }
 
+    private void ClickAndDrag()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = firstPersonCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.distance <= pickUpDistance)
+                {
+                    if (hit.transform.tag != "Ground")
+                    {
+                        Debug.Log("I pick up something");
+                        canPickUp = true;
+                        itemRb = hit.rigidbody;
+                        itemTransform = hit.transform;
+
+
+                        itemRb.useGravity = false;
+                        itemRb.isKinematic = true;
+                        itemTransform.position = guide.position;
+                        itemTransform.rotation = guide.rotation;
+                        itemTransform.parent = guide;
+                    }
+                }
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            if (itemRb != null)
+            {
+                itemRb.useGravity = true;
+                itemRb.isKinematic = false;
+                itemTransform.position = guide.position;
+                itemTransform.parent = null;
+            }
+        }
     }
 }
